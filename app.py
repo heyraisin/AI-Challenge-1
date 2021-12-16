fa13604f5f37a41e2edbd6db5a8da3a29add6ea1
heyraisin <thu.le190203@vnuk.edu.vn>
2021-12-16 21:06:46 +0700

    create new file

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, jsonify
+import base64
+import time
+import numpy as np
+import cv2
+import tensorflow as tf
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/recognize', methods = ['POST'])
+def recognize():
+
+    if request.method == 'POST':
+        print('Receive image and start predicting')
+        data = request.get_json()
+        imageBase64 = data['image']
+        imgBytes = base64.b64decode(imageBase64)
+
+        with open('temp.jpg', 'wb') as temp:
+            temp.write(imgBytes)
+
+        time.sleep(1)
+        # Chay ham predict o day
+        image = cv2.imread('temp.jpg')
+        image = cv2.resize(image, (28, 28), interpolation=cv2.INTER_AREA)
+        img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # print(image)
+        img_predict = (255 - img_gray.reshape(1, 28, 28).astype('float32')) / 255.
+        print(img_predict.shape)
+        print(img_predict)
+
+        # Load trained model
+        classes_name = ['CAKE', 'CAMERA', 'CANDLE']
+        model = tf.keras.models.load_model('quickdraw.h5')
+        print(model.summary())
+        prediction = int(np.argmax(model.predict(img_predict)))
+        print('Model predict: ', model.predict(img_predict))
+        predict_class = classes_name[prediction]
+
+        print('Prediction: ', predict_class)
+        return jsonify({
+            'prediction': str(predict_class),
+            'status': True,
+        })
+
+if __name__ == "__main__":
+    # app.run(host='0.0.0.0', port=81, debug = True)
+    app.run(debug = True)
